61d6368832e14fc9fc428a84bfbf6ef94df5496b
Muhmmad <umer.slysol@gmail.com>
2023-10-23 17:27:02 +0500

    fix: contact layout issues plus error msg fixed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -32,23 +32,19 @@ const Contact = () => {
       >
         <Form />
         <div className="pt-3">
-          <h2 className="text-pink lg:text-2xl text-xl lg:mb-3 md:mb-2 mb-1">
-            Address
-          </h2>
+          <h2 className="text-pink text-xl lg:mb-3 md:mb-2 mb-1">Address</h2>
           <p>Office # 2, 2nd Floor, Icon Center, Beside Panorama Center</p>
           <p>48 Mall Road, Lahore, Pakistan</p>
           <hr className="border-t-2 border-dashed border-gray-200 my-5" />
-          <h2 className="text-pink lg:text-2xl text-xl lg:mb-3 md:mb-2 mb-1">
-            Phone
-          </h2>
-          <a href="tel:+923104150111">+92310-4150111</a>
+          <h2 className="text-pink text-xl lg:mb-3 md:mb-2 mb-1">Phone</h2>
+          <Link href="tel:+923104150111">+92-3104150111</Link>
           <hr className="border-t-2 border-dashed border-gray-200 my-5" />
-          <h2 className="text-pink lg:text-2xl text-xl lg:mb-3 md:mb-2 mb-1">
-            Email
-          </h2>
-          <p>sheikh.haris.zahid@slysol.com</p>
+          <h2 className="text-pink text-xl lg:mb-3 md:mb-2 mb-1">Email</h2>
+          <Link href={'mailto:sheikh.haris.zahid@slysol.com'}>
+            sheikh.haris.zahid@slysol.com
+          </Link>
           <hr className="border-t-2 border-dashed border-gray-200 my-5" />
-          <h2 className="text-pink lg:text-2xl text-xl lg:mb-3 md:mb-2 mb-2">
+          <h2 className="text-pink text-xl lg:mb-3 md:mb-2 mb-2">
             Social Media
           </h2>
           <div className="flex gap-5 text-3xl text-primary2">
