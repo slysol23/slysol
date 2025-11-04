692861d25bcfad470819646a98420a021f0215f8
Umer Riaz <umer.slysol@gmail.com>
2025-11-04 11:29:48 +0500

    feat: Auth js integration login flow

---
+++ src/lib/auth/index.ts
@@ -0,0 +1,30 @@
+import { ApiResponse, IUser, LoginCredentials, RegisterData } from 'lib/type';
+import apiClient from '../client';
+
+export const auth = {
+  login: async (credentials: LoginCredentials) => {
+    const response = await apiClient.post<ApiResponse<boolean>>(
+      '/login',
+      credentials,
+    );
+    return response.data;
+  },
+
+  register: async (userData: RegisterData) => {
+    const response = await apiClient.post<ApiResponse<IUser>>(
+      '/register',
+      userData,
+    );
+    return response.data;
+  },
+
+  getSession: async () => {
+    const response = await apiClient.get<ApiResponse<IUser>>('/session');
+    return response.data;
+  },
+
+  logout: async () => {
+    const response = await apiClient.post<ApiResponse<{}>>('/logout');
+    return response.data;
+  },
+};
