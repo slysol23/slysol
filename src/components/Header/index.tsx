5cabf34c73ed6881c4fad60659f425c634d87016
Umer Riaz <umer.slysol@gmail.com>
2025-11-05 13:03:01 +0500

    refactor: Removed google translate

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -48,6 +48,7 @@ export default function Header({ classes }: HeaderProps) {
     return () => {
       document.removeEventListener('click', handleClick);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const MenuItem = ({
