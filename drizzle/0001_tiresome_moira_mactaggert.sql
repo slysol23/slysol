364fab46201bc4d5f3d4d9a2533d754843098051
hamzaslysol-dot <hamza.slysol@gmail.com>
2025-11-07 15:27:16 +0500

    created blog and author table

--- drizzle/0001_tiresome_moira_mactaggert.sql
+++ drizzle/0001_tiresome_moira_mactaggert.sql
@@ -21,16 +21,4 @@ CREATE TABLE "blog" (
 	CONSTRAINT "blog_id_unique" UNIQUE("id")
 );
 --> statement-breakpoint
-CREATE TABLE "user" (
-	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "user_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
-	"name" text NOT NULL,
-	"email" text NOT NULL,
-	"password" text NOT NULL,
-	"is_admin" boolean DEFAULT false NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "user_id_unique" UNIQUE("id"),
-	CONSTRAINT "user_email_unique" UNIQUE("email")
-);
---> statement-breakpoint
 ALTER TABLE "blog" ADD CONSTRAINT "blog_author_id_author_id_fk" FOREIGN KEY ("author_id") REFERENCES "public"."author"("id") ON DELETE no action ON UPDATE no action;
